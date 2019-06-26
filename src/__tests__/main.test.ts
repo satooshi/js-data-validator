a2b9ef36c413eeb8a84388164ee1f12dc0c32e1e
Kitamura Satoshi <satooshi.jp@gmail.com>
2019-06-26 13:25:59 +0000

    Designing interfaces

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,7 +1,7 @@
-import { validate, Validator } from '../main';
+import { validate, ValidationResult } from '../main';
 
 describe('validate', () => {
-  it('returns Validate instance', () => {
-    expect(validate({ name: 'my name' })).toBeInstanceOf(Validator);
+  it('returns a ValidationResult instance', () => {
+    expect(validate({ name: 'my name' })).toBeInstanceOf(ValidationResult);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,66 @@
-export class Validator {
-  value: object;
 
-  constructor(value: object) {
-    this.value = value;
+/**
+ * Data set validator.
+ */
+export class Validator {
+  dataSet: object;
+
+  constructor(dataSet: object) {
+    this.dataSet = dataSet;
+  }
+
+  /**
+   * Validates the data set.
+   *
+   * @returns {Array<ValidationError>}
+   */
+  validate(): Array<ValidationError> {
+    // TODO Travarse the data set
+    return []; // TODO returns Array<ValidationError>
+  }
+
+  validateValue(value) {}
+}
+
+export class ValidationError {}
+
+/**
+ * The class contains current validation information.
+ */
+export class ValitionContext {
+  dataSet: object;
+  errors: Array<ValidationError>;
+
+  constructor(dataSet: object) {
+    this.dataSet = dataSet;
   }
 }
 
-export function validate(value: object) {
-  return new Validator(value)
+/**
+ * Validation result.
+ */
+export class ValidationResult {
+  validator: Validator;
+
+  constructor(validator: Validator) {
+    this.validator = validator;
+  }
+
+  /**
+   * @returns {boolean} true if the data set is valid, false otherwise.
+   */
+  isValid(): boolean {
+    const errors = this.validator.validate();
+    return errors.length === 0
+  }
+}
+
+/**
+ * Exposed function to validate data set.
+ *
+ * @param dataSet {object} The data set to validate.
+ * @returns {ValidationResult}
+ */
+export function validate(dataSet: object): ValidationResult {
+  return new ValidationResult(new Validator(dataSet));
 }
